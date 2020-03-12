c2bd0afef181f5e5fcf4103ea0497dcdbead6122
Vadim000009 <tuukvadim@live.com>
2020-03-12 17:59:38 +0300

    Thues

--- src/main/java/com/example/web/app/WebAppExample.java
+++ src/main/java/com/example/web/app/WebAppExample.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
 public class WebAppExample {
+    String string;
     public static void main(String[] args) {
         SpringApplication.run(WebAppExample.class, args);
     }
